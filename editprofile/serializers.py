1830b6ee320b0edad90b5f670e40b77f98a9bc3f
mehrangiz baghi <mehrangiz_baghi@comp.iust.ac.ir>
2024-11-14 22:31:19 +0330

    Update : update editprofile app

--- editprofile/serializers.py
+++ editprofile/serializers.py
@@ -3,15 +3,18 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class UserProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'city', 'user_name', 'profile_picture', 'gender', 'bio', 'email',
-                  'password']
+        fields = [
+            'first_name', 'last_name', 'city', 'user_name', 'profile_picture',
+            'gender', 'bio', 'email', 'password'
+        ]
         extra_kwargs = {
             'password': {'write_only': True, 'required': False}
         }
+
+
 
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
--- editprofile/views.py
+++ editprofile/views.py
@@ -1,25 +1,29 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from .serializers import UserProfileUpdateSerializer
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-
 
 class UserProfileUpdateAPIView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
 
-    def put(self, request):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return Response({'detail': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
+
         user = request.user
-        serializer = UserProfileUpdateSerializer(user, data=request.data, partial=True)  # Allow partial updates
+        serializer = UserProfileUpdateSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        if not request.user.is_authenticated:
+            return Response({'detail': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        user = request.user
+        serializer = UserProfileUpdateSerializer(user, data=request.data, partial=True, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
