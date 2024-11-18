9e6dabdebb457339c643e1a23c0c5a69a6dffa5a
fatemehesmaeli <asfaty200@gmail.com>
2024-11-18 11:41:12 -0800

    fix:fix path of image

--- Landing/views.py
+++ Landing/views.py
@@ -8,15 +8,15 @@ from django.conf import settings
 def landing_page(request):
     content = {
         "text": "Start Your",
-        "text3":"ADVENTURE",
-        "text4":"Today !",
+        "text3": "ADVENTURE",
+        "text4": "Today !",
         "login_button": "LogIn",
         "signup_button": "SignUP",
         "paginate": "Let's Go ",
         "home": "Home",
         "about": "About",
         "logo_name": "Trip Tide",
-        "image_url": f"{settings.MEDIA_URL}landing/bus.png",
-        "logo_image_url": f"{settings.MEDIA_URL}landing/logo.png",
+        "image_url": f"https://triptide.pythonanywhere{settings.MEDIA_URL}landing/bus.png",
+        "logo_image_url": f"https://triptide.pythonanywhere{settings.MEDIA_URL}landing/logo.png",
     }
     return Response(content)
