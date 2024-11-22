9e1d6f0920270ff60a6be876864fd61e0aae8763
fatemehesmaeli <asfaty200@gmail.com>
2024-11-21 23:43:47 -0800

    fix:fix datatime to date

--- Travels/models.py
+++ Travels/models.py
@@ -23,13 +23,13 @@ class Travel(models.Model):
     ]
     admin=models.OneToOneField(User,on_delete=models.PROTECT)
     name = models.CharField(max_length=200)
-    start_date = models.DateTimeField(auto_now_add=False)
+    start_date = models.DateField(auto_now_add=False)
     photo = models.ImageField(upload_to="profiles",blank=True,)
     destination = models.CharField(max_length=200)
     mode=models.CharField(max_length=200,choices=type_choices)
     start_place=models.CharField(max_length=200)
     transportation=models.CharField(max_length=200,choices=trans_choices)
-    end_date=models.DateTimeField(auto_now_add=False)   
+    end_date=models.DateField(auto_now_add=False)   
     travellers=models.IntegerField(blank=False,default=5)
     def __str__(self):
         return self.destination
