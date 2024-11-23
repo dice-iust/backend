d6e3e240989328b0a68b7cfd17906704b7da8d5c
fatemehesmaeli <asfaty200@gmail.com>
2024-11-23 04:16:38 -0800

    fix:fix for maping with front

--- editprofile/urls.py
+++ editprofile/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path
-from .views import UserProfileUpdateAPIView
+from .views import UserProfileUpdateAPIView,putmethod
 
 urlpatterns = [
-    path('update/', UserProfileUpdateAPIView.as_view(), name='user-profile-update'),
+    path("update/", UserProfileUpdateAPIView.as_view(), name="user-profile-update"),
+    path("update_2/", putmethod.as_view()),
 ]
-
