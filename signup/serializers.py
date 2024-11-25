15a64fd36c04aaeb3454208556abfae58bfb7985
mehrangiz baghi <mehrangiz_baghi@comp.iust.ac.ir>
2024-11-25 11:25:44 +0330

    ADD: add forgot password

--- signup/serializers.py
+++ signup/serializers.py
@@ -3,6 +3,13 @@ from django.contrib.auth import get_user_model
 from datetime import date
 import re
 
+from rest_framework.exceptions import ValidationError
+
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+from .models import *
 User = get_user_model()
 
 
@@ -10,27 +17,42 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         max_length=100, min_length=6, style={"input_type": "password"}
     )
+    confirmPassword = serializers.CharField(max_length=100, min_length=6, style={"input_type": "password"})
 
     class Meta:
         model = User
-        fields = ["user_name", "email", "password"]
+        fields = ["user_name", "email", "password", "confirmPassword"]
 
-    def create(self, validated_data):
-        user_password = validated_data.pop("password")
-        if not user_password or len(user_password) < 6:
-            raise serializers.ValidationError("Password must be at least 6 characters")
-        if not re.search(r"[A-Za-z]", user_password):
-            raise serializers.ValidationError(
-                "Password must contain at least one letter"
-            )
-        if not re.search(r"[0-9]", user_password):
-            raise serializers.ValidationError(
-                "Password must contain at least one number"
-            )
-        user = User(**validated_data)
-        user.set_password(user_password)
-        user.save()
-        return user
+    # def create(self, validated_data):
+    #     user_password = validated_data.pop("password")
+    #     validated_data.pop("confirmPassword")
+    #     if not user_password or len(user_password) < 6:
+    #         raise serializers.ValidationError("Password must be at least 6 characters")
+    #     if not re.search(r"[A-Za-z]", user_password):
+    #         raise serializers.ValidationError(
+    #             "Password must contain at least one letter"
+    #         )
+    #     if not re.search(r"[0-9]", user_password):
+    #         raise serializers.ValidationError(
+    #             "Password must contain at least one number"
+    #         )
+    #     user = User(**validated_data)
+    #     user.set_password(user_password)
+    #     user.save()
+    #     return user
+
+    def validate(self, data):
+        password = data.get('password')
+        confirmPassword = data.get('confirmPassword')
+        if not confirmPassword:
+            raise serializers.ValidationError("Please enter confirm password")
+        if not password:
+            raise serializers.ValidationError("Please enter password")
+        if password != confirmPassword:
+            raise serializers.ValidationError("Password and confirm password are not equal")
+
+        else:
+            return data
 
 
 class UserLoginSerializer(serializers.Serializer):
@@ -58,3 +80,35 @@ class UserViewSerializer(serializers.ModelSerializer):
     #         )
     #         return age
     #     return None
+
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailVerification
+        fields = ["verification_code","email"]
+    def validate(self, obj):
+        if  obj.is_expired():
+            raise ValidationError("the verification code is expired")
+        else:
+            return obj
+
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate(self, data):
+        email = data.get('email')
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("No user found with this email address.")
+        return data
+class PasswordResetSerializer(serializers.Serializer):
+    new_password = serializers.CharField(max_length=100, min_length=6, write_only=True)
+    confirm_password = serializers.CharField(max_length=100, min_length=6, write_only=True)
+    def validate(self, data):
+        new_password = data.get("new_password")
+        confirm_password = data.get("confirm_password")
+        if new_password != confirm_password:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+
--- signup/views.py
+++ signup/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .serializers import (
     UserRegistrationSerializer,
     UserLoginSerializer,
-    UserViewSerializer,
+    UserViewSerializer, EmailVerificationSerializer, ForgotPasswordSerializer, PasswordResetSerializer
 )
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -15,6 +16,14 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 import jwt
 from .generate import generate_access_token
+import random
+from django.core.mail import send_mail
+from .models import EmailVerification
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 User = get_user_model()
 
@@ -22,7 +31,7 @@ User = get_user_model()
 class UserRegistrationAPIView(APIView):
     serializer_class = UserRegistrationSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny,]
 
     def get(self, request):
         content = {"message": "Hello!"}
@@ -33,32 +42,32 @@ class UserRegistrationAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             # Check if the user already exists
             if User.objects.filter(
-                user_name=serializer.validated_data["user_name"]
+                    user_name=serializer.validated_data["user_name"]
             ).exists() and not User.objects.filter(email=serializer.validated_data["email"]).exists():
                 return Response(
-                    {"error": "This username already exists."},
+                    {"error": "This user_name already exists."},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
             if (
-                User.objects.filter(email=serializer.validated_data["email"]).exists()
-                and not User.objects.filter(
-                    user_name=serializer.validated_data["user_name"]
-                ).exists()
+                    User.objects.filter(email=serializer.validated_data["email"]).exists()
+                    and not User.objects.filter(
+                user_name=serializer.validated_data["user_name"]
+            ).exists()
             ):
                 return Response(
                     {"error": "This email already exists."},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-                
+
             if (
-                User.objects.filter(email=serializer.validated_data["email"]).exists()
-                and User.objects.filter(
-                    user_name=serializer.validated_data["user_name"]
-                ).exists()
+                    User.objects.filter(email=serializer.validated_data["email"]).exists()
+                    and User.objects.filter(
+                user_name=serializer.validated_data["user_name"]
+            ).exists()
             ):
                 return Response(
-                    {"error": "This username and email already exists."},
+                    {"error": "This user_name and email already exist."},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
@@ -77,19 +86,23 @@ class UserRegistrationAPIView(APIView):
 class UserLoginAPIView(APIView):
     serializer_class = UserLoginSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
-
+    permission_classes = [AllowAny,]
+    def get(self, request):
+        photo_response = {
+            "photo": f"https://triptide.pythonanywhere.com{settings.MEDIA_URL}login.jpg"
+        }
+        return Response(photo_response)
     def post(self, request):
         user_name = request.data.get("user_name", None)
         user_password = request.data.get("password", None)
 
         if not user_password:
-            raise AuthenticationFailed("A user password is needed.")
+            raise AuthenticationFailed("A password is needed.")
 
         if not user_name:
-            raise AuthenticationFailed("An user user name is needed.")
+            raise AuthenticationFailed("A user_name is needed.")
 
-        user_instance = authenticate(username=user_name, password=user_password)
+        user_instance = authenticate(user_name=user_name, password=user_password)
 
         if not user_instance:
             raise AuthenticationFailed("User not found.")
@@ -107,7 +120,7 @@ class UserLoginAPIView(APIView):
 
 class UserViewAPI(APIView):
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny,]
 
     def get(self, request):
         user_token = request.COOKIES.get("access_token")
@@ -125,7 +138,7 @@ class UserViewAPI(APIView):
 
 class UserLogoutViewAPI(APIView):
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny,]
 
     def get(self, request):
         user_token = request.COOKIES.get("access_token", None)
@@ -137,3 +150,173 @@ class UserLogoutViewAPI(APIView):
         response = Response()
         response.data = {"message": "User is already logged out."}
         return response
+
+
+class UserRegistrationAndVerificationAPIView(APIView):
+    serializer_class = UserRegistrationSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [AllowAny,]
+
+    def get(self, request):
+        content = {"message": "Hello!"}
+        return Response(content)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+
+            if (
+                User.objects.filter(
+                    user_name=serializer.validated_data["user_name"]
+                ).exists()
+                and not User.objects.filter(
+                    email=serializer.validated_data["email"]
+                ).exists()
+            ):
+                return Response(
+                    {"error": "This user_name already exists."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            if (
+                User.objects.filter(email=serializer.validated_data["email"]).exists()
+                and not User.objects.filter(
+                    user_name=serializer.validated_data["user_name"]
+                ).exists()
+            ):
+                return Response(
+                    {"error": "This email already exists."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            if (
+                User.objects.filter(email=serializer.validated_data["email"]).exists()
+                and User.objects.filter(
+                    user_name=serializer.validated_data["user_name"]
+                ).exists()
+            ):
+                return Response(
+                    {"error": "This user_name and email already exist."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            verification_code = str(random.randint(100000, 999999))
+            if not serializer.validated_data["password"] or len(serializer.validated_data["password"]) < 6:
+                raise serializers.ValidationError("Password must be at least 6 characters")
+            if not re.search(r"[A-Za-z]", serializer.validated_data["password"]):
+                return Response(
+                    "Password must contain at least one letter"
+                )
+            if not re.search(r"[0-9]", serializer.validated_data["password"]):
+                return Response("Password must contain at least one number")
+            EmailVerification.objects.create(
+                email=serializer.validated_data["email"],
+                username=serializer.validated_data["user_name"],
+                password=serializer.validated_data["password"],
+                verification_code=verification_code,
+            )
+            send_mail(
+                subject="Your Verification Code",
+                message=f"Your verification code is: {verification_code}",
+                from_email="triiptide@gmail.com",
+                recipient_list=[serializer.validated_data["email"]],
+            )
+            return Response({"email": serializer.validated_data["email"]})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EmailVerificationView(APIView):
+    serializer = EmailVerificationSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [TokenAuthentication,]
+
+    def post(self, request, *args, **kwargs):
+        email_serializer = self.serializer(data=request.data)
+
+
+        if email_serializer.is_valid():
+            email = email_serializer.validated_data["email"]
+            verification_send_code = email_serializer.validated_data[
+                "verification_code"
+            ]
+
+          
+            verification = EmailVerification.objects.filter(email=email).last()
+
+            if not verification:
+                return Response(
+                    {"error": "No verification record found."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            if str(verification.verification_code) == str(verification_send_code):
+                new_user = User.objects.create(
+                    user_name=verification.username,
+                    email=verification.email,
+                    password=verification.password,
+                )
+                new_user.set_password(verification.password)
+                new_user.save()
+                access_token = generate_access_token(new_user)
+                data = {"access_token": access_token}
+                response = Response(data, status=status.HTTP_201_CREATED)
+                response.set_cookie(
+                    key="access_token", value=access_token, httponly=True
+                )
+                return response
+
+            return Response(
+                {"error": "Invalid verification code."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+        return Response(email_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#forgot_password:
+class ForgotPasswordView(APIView):
+    permission_classes = (AllowAny),
+    serializer_class = ForgotPasswordSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            user = User.objects.get(email=email)
+            token = PasswordResetTokenGenerator().make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+
+            reset_link = request.build_absolute_uri(
+                reverse('password-reset-confirm', kwargs={'uidb64': uid, 'token': token})
+            )
+
+            send_mail(
+                subject="Password Reset Request",
+                message=f"Click the link to reset your password: {reset_link}",
+                from_email="noreply@yourdomain.com",
+                recipient_list=[email],
+            )
+            return Response({"message": "Password reset link sent to your email."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PasswordResetConfirmView(APIView):
+    permission_classes = [AllowAny],
+    serializer_class = PasswordResetSerializer
+
+    def post(self, request, uidb64, token):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            try:
+                uid = force_str(urlsafe_base64_decode(uidb64))
+                user = User.objects.get(pk=uid)
+
+                if not PasswordResetTokenGenerator().check_token(user, token):
+                    return Response({"error": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+
+                new_password = serializer.validated_data['new_password']
+                user.set_password(new_password)
+                user.save()
+                return Response({"message": "Password reset successful."}, status=status.HTTP_200_OK)
+            except (User.DoesNotExist, ValueError):
+                return Response({"error": "Invalid user."}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
