31901c2b2cec8cbad4b035f1f4dad787b466a91b
mehrangiz baghi <mehrangiz_baghi@comp.iust.ac.ir>
2024-11-25 13:18:19 +0330

    fix:fix feilds with front

--- editprofile/serializers.py
+++ editprofile/serializers.py
@@ -4,21 +4,19 @@ import re
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
 class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    currentPassword = serializers.CharField(required=True, write_only=True)
+    newPassword = serializers.CharField(required=False, write_only=True)
+    # confirmPassword = serializers.CharField(required=False, write_only=True)
+
     class Meta:
         model = User
         fields = [
-            'firstName', 'lastName', 'city', 'user_name', 'profilePicture',
-            'gender', 'bio', 'email', 'phone', 'birthDate', 'password'
+            'firstName', 'lastName', 'city', 'user_name', 'profilePicture','currentPassword', 'newPassword',
+            'gender', 'bio', 'email', 'phone', 'birthDate'
         ]
-        extra_kwargs = {
-            'password': {'write_only': True, 'required': False},
-        }
 
-    def validate_email(self, value):
-        if "@" not in value:
-            raise serializers.ValidationError("The email must contain '@'.")
-        return value
 
     def validate_password(self, value):
         if value:
@@ -31,18 +29,43 @@ class UserProfileUpdateSerializer(serializers.ModelSerializer):
             if value.isdigit() or value.isalpha():
                 raise serializers.ValidationError("Password cannot be all letters or all numbers.")
         return value
-
     def validate(self, data):
-        password = data.get('password')
-        if password:
-            self.validate_password(password)
+        currentPassword = data.get('currentPassword')
+        newPassword = data.get('newPassword')
+
+        if newPassword:
+            if 'confirmPassword' in data and data.get('confirmPassword') != newPassword:
+                raise serializers.ValidationError("New password and confirm password must match.")
+
+            # Ensure current password is correct
+        if currentPassword:
+            user = self.context['request'].user
+            if not user.check_password(currentPassword):
+                raise serializers.ValidationError("Current password is incorrect.")
+
         return data
+    def get_profilePicture(self, obj):
+        if obj.profilePicture:
+            return obj.profilePicture.url
+        return None  # You could return a default image URL if desired
 
-
+    def validate_email(self, value):
+        if "@" not in value:
+            raise serializers.ValidationError("The email must contain '@'.")
+        return value
 
     def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
+        currentPassword = validated_data.pop('currentPassword', None)
+        newPassword = validated_data.pop('newPassword', None)
 
+        # Check if currentPassword is correct and change to new password
+        if currentPassword:
+            if not instance.check_password(currentPassword):
+                raise serializers.ValidationError("Current password is incorrect.")
+            if newPassword:
+                instance.set_password(newPassword)
+
+        password = validated_data.pop('password', None)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
 
@@ -51,3 +74,4 @@ class UserProfileUpdateSerializer(serializers.ModelSerializer):
 
         instance.save()
         return instance
+
