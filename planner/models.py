671a8b35dc8e4d5abed85b473480aede89171a6d
fatemehesmaeli <asfaty200@gmail.com>
2024-12-04 06:32:33 -0800

    fix:fix planner get

--- planner/models.py
+++ planner/models.py
@@ -13,8 +13,8 @@ class Expense(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_expenses")
 
-    def split_amount(self):
-        return self.amount / self.participants.count()
+    # def split_amount(self):
+    #     return self.amount / self.participants.count()
 
 class Settlement(models.Model):
     payer = models.ForeignKey(User, on_delete=models.PROTECT, related_name="payer_settlements")
