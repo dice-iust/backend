849bbfa5579670a2bf9e19296fa0cedc8f9edda6
fatemehesmaeli <asfaty200@gmail.com>
2024-12-11 11:13:53 -0800

    fix:fix some chat errors

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,41 +5,45 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from channels.db import database_sync_to_async
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
-from django.conf import settings
+import logging
 
+logger = logging.getLogger(__name__)
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.travel_name = self.scope["url_route"]["kwargs"]["travel_name"]
-        self.room_group_name = f"travel_{self.travel_name}"
+        try:
+            self.travel_name = self.scope["url_route"]["kwargs"]["travel_name"]
+            self.room_group_name = f"travel_{self.travel_name}"
 
-        headers = dict(self.scope.get("headers", []))
-        auth_header = headers.get(b"Authorization", None)
-        if not auth_header:
+            headers = dict(self.scope.get("headers", []))
+            auth_header = headers.get(b"Authorization", None)
+            if not auth_header:
+                await self.close()
+                return
+
+            token = auth_header.decode("utf-8").split(" ")[-1]
+            user = await self.get_user_from_token(token)
+
+            if not user:
+                await self.close()
+                return
+
+            travel, travellers_group = await self.get_travel_and_group(
+                self.travel_name, user
+            )
+            if not travel or not travellers_group:
+                await self.close()
+                return
+
+            self.user = user
+            self.travellers_group = travellers_group
+
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            await self.accept()
+        except Exception as e:
+            logger.error(f"Error in connect: {e}")
             await self.close()
-            return
-
-        token = auth_header.decode("utf-8").split(" ")[-1] 
-        user = await self.get_user_from_token(token)
-
-        if not user:
-            await self.close()
-            return
-
-
-        travel, travellers_group = await self.get_travel_and_group(
-            self.travel_name, user
-        )
-        if not travel or not travellers_group:
-            await self.close()
-            return
-
-        self.user = user
-        self.travellers_group = travellers_group
-
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
@@ -49,7 +53,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json["message"]
 
         await self.save_message(message)
-
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -64,14 +67,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = event["message"]
         user_name = event["user_name"]
 
-    
         await self.send(
             text_data=json.dumps({"message": message, "user_name": user_name})
         )
 
     @database_sync_to_async
     def save_message(self, message):
-
         from Travels.models import ChatMessage
 
         ChatMessage.objects.create(
@@ -86,11 +87,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             user_id = payload.get("user_id")
-            return settings.AUTH_USER_MODEL.objects.filter(user_id=user_id).first()
+            return get_user_model().objects.filter(id=user_id).first()
         except ExpiredSignatureError:
-            return None 
+            logger.warning("Token has expired.")
+            return None
         except InvalidTokenError:
-            return None 
+            logger.warning("Invalid token.")
+            return None
 
     @database_sync_to_async
     def get_travel_and_group(self, travel_name, user):
