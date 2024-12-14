68e9ed67213dd7899f8edae84fa0a650734978c2
fatemehesmaeli <asfaty200@gmail.com>
2024-12-14 10:36:45 -0800

    fix :fix some errors in chating

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 from .views import *
+from . import consumers
 
 urlpatterns = [
     path("chat/<str:travel_name>/messages/",ChatMessageView.as_view()),
-]
-from django.urls import re_path
-from .consumers import ChatConsumer  # Ensure this points to your consumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<travel_name>\w+)/$',ChatConsumer.as_asgi()),
 ]
