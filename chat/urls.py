3111bdad6ceae599813493fed9f632815c95c652
fatemehesmaeli <asfaty200@gmail.com>
2024-12-21 10:28:48 -0800

    add:add new way for chat

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,15 @@
+# from django.urls import path
+# from .views import *
+# from . import consumers
+# from .views_consumer import AblyMessagePublishView
+# urlpatterns = [
+#     path("chat/messages/",ChatMessageView.as_view()),
+#     path('publish_message/', AblyMessagePublishView, name='publish_message'),
+# ]
 from django.urls import path
-from .views import *
-from . import consumers
-from .views_consumer import AblyMessagePublishView
+from .views_consumer import post_message, message_stream
+
 urlpatterns = [
-    path("chat/messages/",ChatMessageView.as_view()),
-    path('publish_message/', AblyMessagePublishView, name='publish_message'),
+    path('api/chat/message/', post_message, name='post_message'),
+    path('events/chat/<str:travel_name>/', message_stream, name='message_stream'),
 ]
