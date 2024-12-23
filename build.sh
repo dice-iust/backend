337272efb8e845d0fee03450f117114801619b90
fatemehesmaeli <asfaty200@gmail.com>
2024-12-23 02:24:21 -0800

    fix:fix for liara

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+daphne -b 0.0.0.0 -p $PORT BackEnd_TravelPlanning.asgi:application
